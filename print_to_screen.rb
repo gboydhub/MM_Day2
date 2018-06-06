78bb722b19a7a9fb0b1f617b80a9004b06d9c7e1
Eponick <eponickstream@gmail.com>
2018-06-06 14:35:04 -0400

    Commented code

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -14,19 +14,19 @@
 
 # printnumbers()
 
-i = 1
-while i <= 100 do
-    if (i%3==0) && (i%5==0)
+i = 1                           # Declare variable i
+while i <= 100 do               # Loop while i less than or equals 100
+    if (i%3==0) && (i%5==0)     # If i is divisiable by 3 and 5
         print "mined minds "
-    elsif i % 3 == 0
+    elsif i % 3 == 0            # Else, if i is divisable by 3
         print "mined "
-    elsif i % 5 == 0
+    elsif i % 5 == 0            # Else, if i is divisable by 5
         print "minds " 
     else
-        print "#{i} "
+        print "#{i} "           # Default case handler
     end
-    i+=1
-    if i % 20 == 0 then
+    i+=1                        # Increment i by 1
+    if i % 20 == 0 then         # Line break every 20 loops
         print "\n"
     end
 end
