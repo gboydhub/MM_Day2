a7f98afd32890c8936209e9e990230c7fad4b356
Eponick <eponickstream@gmail.com>
2018-06-06 12:11:23 -0400

    Initial upload

---
+++ print_to_screen.rb
@@ -0,0 +1,40 @@
+
+# for i in 0..5 do
+#     print i
+#     if i < 5 then
+#         print ", "
+#     end
+# end
+
+# def printnumbers()
+#     100.times do
+#         print 1
+#     end
+# end
+
+# printnumbers()
+
+i = 1
+while i <= 100 do
+    if i == 3
+        print "mined "
+    elsif i == 4
+        print "minds " 
+    else
+        print "#{i} "
+    end
+    i+=1
+    if i % 10 == 0 then
+        print "\n"
+    end
+end
+
+int main()
+{
+    int i = 0;
+    while(i < 100)
+        {
+            printf("%d", i);
+            i++;
+        }
+}
